3cc4691120a3aedab0632fb977545d89d9fb9450
Espacio-Root <Abdullahd00124@gmail.com>
2023-07-16 23:02:01 +0300

    Updated File Structure

--- multiple.py
+++ multiple.py
@@ -1,9 +1,14 @@
-from main import MultipleInstances
+from single import MultipleInstances
+import sys
 import os
 
-if __name__ == '__main__':
-
+def run(time):
+    
     website_list_path = 'website_list.txt'
     website_list_path = os.path.join(os.path.dirname(__file__), website_list_path)
     
-    MultipleInstances(10, 'website_list.txt', 60, 1).block_websites()
+    MultipleInstances(10, 'website_list.txt', time, 1).block_websites()
+
+if __name__ == '__main__':
+    time_to_unblock = float(sys.argv[1])
+    run(time_to_unblock)
