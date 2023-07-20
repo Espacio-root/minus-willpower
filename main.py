8ea80194bc9a2bff750c6830eb4e17b5be750cf9
Espacio-Root <Abdullahd00124@gmail.com>
2023-07-21 00:32:50 +0300

    Cleaned up the code. Final Update

--- main.py
+++ main.py
@@ -7,36 +7,18 @@ if __name__ == '__main__':
 
     max_time_period = 24 * 60 * 60
 
-    website_list_path = 'websites.txt'
+    website_file_name = 'websites.txt'
+    website_list_path = os.path.join(os.path.dirname(__file__), website_file_name)
     delay_between_checks = 2
     arg = sys.argv[1]
-    time = int(arg) if int(arg) > time() and int(arg) < time() + max_time_period else min(Helper.handle_time(arg), Helper.handle_time(max_time_period))
+    try:
+        time = float(arg) if float(arg) > time() and float(arg) < time() + max_time_period else 0
+    except:
+        time = min(Helper.handle_time(arg), Helper.handle_time(max_time_period))
     
     instance = TerminalPreventBlocker(website_list_path, time, delay_between_checks * 2)
     
-    # get c dir
-    user_dir = os.path.join(os.path.expanduser('~'), r"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup", 'minus-willpower.bat')
-    cur_dir = os.path.join(os.path.dirname(__file__), 'main.py')
-    content = f'@echo off\npythonw "{cur_dir}" {time}'
-
-    if not os.path.exists(user_dir):
-        os.makedirs(user_dir)
-
-    with open(user_dir, 'w') as fp:
-        fp.write(content)
-
     for _ in range(2):
         instance.launch_instance(delay_between_checks)
         sleep(delay_between_checks)
         
-    while True:
-        if not os.path.exists(user_dir):
-            os.makedirs(user_dir)
-
-        with open(user_dir, 'r') as fp:
-            new_content = fp.read()
-        
-        if new_content != content:
-            with open(user_dir, 'w') as fp:
-                fp.write(content)
-                
--- single.py
+++ single.py
@@ -2,30 +2,34 @@ import platform
 from time import time, sleep
 import sys
 import os
+import stat
 import subprocess
 import psutil
 from threading import Thread
 
+
 class WebsiteBlocker:
-    
+
     def __init__(self, website_list_path):
         self.hosts_path = self._initialize_host_path()
         self.website_list_path = website_list_path
 
         initial_website_list = self.website_list
-        self.website_list_content = lambda website_list: [f'127.0.0.1 {website}\n' for website in website_list if website.strip() != '' and '#' not in website]
-        self.initial_website_content = ''.join(self.website_list_content(initial_website_list))
-        
+        self.website_list_content = lambda website_list: [
+            f'127.0.0.1 {website}\n' for website in website_list if website.strip() != '' and '#' not in website]
+        self.initial_website_content = ''.join(
+            self.website_list_content(initial_website_list))
+
     @property
     def hosts_content(self):
         with open(self.hosts_path, 'r') as fp:
             return fp.read()
-    
+
     @property
     def website_list(self):
         with open(self.website_list_path, 'r') as fp:
             return fp.read().split('\n')
-        
+
     def _initialize_host_path(self):
         system = platform.system()
 
@@ -41,9 +45,10 @@ class WebsiteBlocker:
         # pattern = r'^127\.0\.0\.1\s+\S+'
         # matches = re.findall(pattern, content, re.MULTILINE)
         cur_website_list = self.website_list_content(self.website_list)
-        updated_list = [website for website in cur_website_list if website not in self.initial_website_content]
+        updated_list = [
+            website for website in cur_website_list if website not in self.initial_website_content]
         self.initial_website_content += ''.join(updated_list)
-            
+
         if hosts_content != self.initial_website_content:
             with open(self.hosts_path, 'w') as host_file:
                 host_file.write(self.initial_website_content)
@@ -52,87 +57,106 @@ class WebsiteBlocker:
 class ConstantWebsiteBlocker(WebsiteBlocker):
     def __init__(self, website_list_path, time_to_unblock, delay_between_checks):
         super().__init__(website_list_path)
-        
+
         self.time_to_unblock = float(time_to_unblock)
         self.delay = int(delay_between_checks)
-        
+
     def block_websites(self):
         while time() <= self.time_to_unblock:
             super().block_websites()
             sleep(self.delay)
-            
+
+
 class TerminalPreventBlocker(ConstantWebsiteBlocker):
     def __init__(self, website_list_path, time_to_unblock, delay_between_checks, track_delay=0):
         super().__init__(website_list_path, time_to_unblock, delay_between_checks)
 
         self.track_delay = track_delay
-        self.launch_instance = lambda x: subprocess.Popen(['pythonw', os.path.join(os.path.dirname(__file__), 'single.py'), str(self.website_list_path), str(self.time_to_unblock), str(self.delay), str(x)])
-        self.cur_script_path = os.path.join(os.path.dirname(__file__), 'single.py')
+        self.launch_instance = lambda x: subprocess.Popen(['python', os.path.join(os.path.dirname(
+            __file__), 'single.py'), str(self.website_list_path), str(self.time_to_unblock), str(self.delay), str(x)])
+        self.cur_script_path = os.path.join(
+            os.path.dirname(__file__), 'single.py')
         self.initial_script_content = self.get_script_content()
 
     def get_script_content(self):
         if not os.path.exists(self.cur_script_path):
             os.makedirs(self.cur_script_path)
             return ''
-        
+
         with open(self.cur_script_path, 'r') as fp:
             return fp.read()
 
-    def verify_file_integrity(self):
+    def verify_script_integrity(self):
         cur_file_content = self.get_script_content()
         if cur_file_content != self.initial_script_content:
             with open(self.cur_script_path, 'w') as fp:
                 fp.write(self.initial_script_content)
-    
+
     def track_instances(self):
         def pythonw_instances():
             instances = []
             for proc in psutil.process_iter(['name', 'pid']):
-                if proc.info['name'] == 'pythonw.exe':  # type: ignore
-                    instances.append(proc.info['pid']) # type: ignore
+                if proc.info['name'] == 'python.exe':  # type: ignore
+                    instances.append(proc.info['pid'])  # type: ignore
             return instances
-        
+
         initial_time = time()
         while time() - initial_time <= self.track_delay * 2:
             continue
         prev_instances = pythonw_instances()
-        
+
         while time() <= self.time_to_unblock:
             cur_instances = pythonw_instances()
             for instance in prev_instances:
                 if instance not in cur_instances:
-                    self.verify_file_integrity()
+                    self.verify_script_integrity()
                     self.launch_instance(0)
                     prev_instances = cur_instances
-    
+            self.verify_bat_integrity()
+
+    def verify_bat_integrity(self):
+        user_dir = fr'{os.path.expanduser("~")}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\minus-willpower.bat'
+        cur_file = os.path.abspath(__file__)
+        content = f'pythonw "{cur_file}" {self.time_to_unblock}'
+
+        if not os.path.exists(user_dir):
+            os.makedirs(user_dir)
+
+        with open(user_dir, 'r') as fp:
+            cur_content = fp.read()
+
+        if cur_content != content:
+            with open(user_dir, 'w') as fp:
+                fp.write(content)
+
     def block_websites(self):
         tracker = Thread(target=self.track_instances)
         tracker.start()
         super().block_websites()
         
 class Helper:
-    
+
     @staticmethod
     def handle_time(cur_time: str | int | float) -> float:
         if isinstance(cur_time, str):
             if ':' not in cur_time:
                 return float(cur_time)
-            
+
             li = cur_time.split(':')
             li.reverse()
             li = [li[i] + '*60' * i for i in range(len(li))]
             cur_time = '+'.join(li)
-            
 
             cur_time = eval(cur_time)
-        
+
         return float(cur_time) + time()
-            
+
+
+
 if __name__ == '__main__':
-    
+
     args = sys.argv[1:]
 
     # website_list_path, time_to_unblock, delay_between_checks
     blocker = TerminalPreventBlocker(args[0], args[1], args[2], int(args[3]))
     blocker.block_websites()
-            
